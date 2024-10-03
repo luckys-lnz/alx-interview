5ac0d2be0b2db94f568b313fdcd1dd5c5db9168b
luckys-lnz <lalucky22@gmail.com>
2024-10-03 13:35:51 +0000

    Pascals triangle solved

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -18,17 +18,16 @@ def pascal_triangle(n):
     triangle = []
 
     for row in range(n):
-        # Start each row with 1
-        current_row = [1] * (row + 1)
+        # Start each row with an empty list
+        current_row = [1]  # manually set the first elemnt to 1.
         
         # Calculate the values for the current row using the values from the previous row
-        for col in range(1, row):
-            current_row[col] = triangle[row - 1][col - 1] + triangle[row - 1][col]
+        if row > 0:
+            for col in range(1, row):
+                current_row.append(triangle[row - 1][col - 1] + triangle[row - 1][col])
+            current_row.append(1)  # Last element is always 1
         
         # Append the current row to the triangle
         triangle.append(current_row)
     
     return triangle
-
-# Example usage:
-print(pascal_triangle(5))
