2065d1ae972317864f5d16375eb3b3bbf9d79bae
luckys-lnz <lalucky22@gmail.com>
2024-10-13 22:59:45 +0000

    0. Lockboxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,20 +1,25 @@
 #!/usr/bin/python3
 
 def canUnlockAll(boxes):
-    # use set to keep track of unlocked boxes
+
+    # if boxes is empty.
+    if not boxes or len(boxes) == 1:
+        return True
+    
+    # track unlocked boxes
     unlocked = set()
     # Start with the first box (index 0)
     unlocked.add(0)
     
     # Use list to keep track of the keys we have
-    keys = boxes[0]
+    keys = list(boxes[0])
     
 
     while keys:
         key = keys.pop()
         
         # If the key is a valid box number and it hasn't been unlocked yet
-        if key not in unlocked and key < len(boxes):
+        if key not in unlocked and 0 <= key < len(boxes):
             unlocked.add(key)
             
             # Add the keys from newly unlocked box to our list of keys
