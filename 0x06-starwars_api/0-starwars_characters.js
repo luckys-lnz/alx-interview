c79145d8c24c719c864f2c50f714b651bdcbeb32
luckys-lnz <lalucky22@gmail.com>
2024-11-16 19:37:38 +0000

    starwars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,53 +1,20 @@
 #!/usr/bin/node
 
-const request = require('request');
-const API_URL = 'https://swapi.dev/api';
+const request = require("request");
 
-// Get the movie ID from command-line arguments
-const movieId = process.argv[2];
-
-if (!movieId) {
-  console.error('Usage: ./0-starwars_characters.js <movie_id>');
-  process.exit(1);
-}
-
-// Function to fetch and display the characters of the specified movie
-function getStarWarsCharacters (movieId) {
-  const movieUrl = `${API_URL}/films/${movieId}/`;
-
-  request(movieUrl, (err, _, body) => {
-    if (err) {
-      console.error('Error fetching movie:', err);
-      return;
-    }
-
-    let movieData;
-    try {
-      movieData = JSON.parse(body);
-    } catch (parseErr) {
-      console.error('Error parsing movie data:', parseErr);
-      return;
-    }
-
-    // Fetch each character's name in order
-    movieData.characters.forEach((characterUrl) => {
-      request(characterUrl, (charErr, __, charBody) => {
-        if (charErr) {
-          console.error('Error fetching character:', charErr);
-          return;
-        }
-
-        let characterData;
-        try {
-          characterData = JSON.parse(charBody);
-          console.log(characterData.name);
-        } catch (charParseErr) {
-          console.error('Error parsing character data:', charParseErr);
-        }
-      });
-    });
+request(
+  "https://swapi.dev/api/films/" + process.argv[2],
+  function (err, __, body) {
+    if (err) throw err;
+    const actors = JSON.parse(body).characters;
+    exactOrder(actors, 0);
+  }
+);
+const exactOrder = (actors, x) => {
+  if (x === actors.length) return;
+  request(actors[x], function (err, __, body) {
+    if (err) throw err;
+    console.log(JSON.parse(body).name);
+    exactOrder(actors, x + 1);
   });
-}
-
-// Call the function to fetch and display the characters
-getStarWarsCharacters(movieId);
+};
