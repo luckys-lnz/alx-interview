9d2f59ace15c0769882359f597a85585e7c11c27
luckys-lnz <lalucky22@gmail.com>
2024-11-18 10:50:16 +0000

    gen

--- 0x03-log_parsing/0-generator.py
+++ 0x03-log_parsing/0-generator.py
@@ -11,7 +11,7 @@ for i in range(10000):
         .format(
             random.randint(1, 255), random.randint(1, 255),
             random.randint(1, 255), random.randint(1, 255),
-            datetime.datetime.now(),
+            datetime.datetime.now().strftime("%d/%b/%Y:%H:%M:%S %z"),
             random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
             random.randint(1, 1024)
         )
